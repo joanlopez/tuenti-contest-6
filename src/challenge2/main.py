60a09fe0cbe675d9a63a6745aea91e0ca25914e7
Joan Lopez <joanjan14@gmail.com>
2016-04-26 11:38:09 +0200

    Solution for challenge2

---
+++ src/challenge2/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+
+def print_output_line(case, words):
+    print("Case #" + str(case) + ": " + words[0][0] + " " + str(words[0][1]) + ","
+          + words[1][0] + " " + str(words[1][1]) + ","
+          + words[2][0] + " " + str(words[2][1]))
+
+
+def get_words_from_text_file(filename):
+    f = open(filename, 'r')
+    return [x for y in [l.split() for l in f.readlines()] for x in y]
+
+
+def get_effective_words(words, start, end):
+    return words[start - 1:end]
+
+
+def get_most_frequency_words(words, num_of_words):
+    return sorted([(w, words.count(w)) for w in set(words)], key=lambda z: z[1], reverse=True)[:num_of_words]
+
+
+def main():
+    # Reading the manuscript
+    words = get_words_from_text_file('corpus.txt')
+
+    # Treating cases
+    num_of_cases = int(raw_input())
+    for i in range(num_of_cases):
+        # Getting start and end
+        edges = raw_input()
+        start = int(edges.split()[0])
+        end = int(edges.split()[1])
+
+        # Retrieving most frequency words
+        effective_words = get_effective_words(words, start, end)
+        most_frequency = get_most_frequency_words(effective_words, 3)
+
+        print_output_line(i, most_frequency)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_challange2.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from src.challenge2.main import *
+
+
+class TestChallange2(TestCase):
+    def test_that_reading_correctly_words_from_file(self):
+        words = get_words_from_text_file("../src/challenge2/testCorpus.txt")
+        self.assertEquals(["Blue", "White", "Aqua", "Azure", "Beige", "Lavender"], words)
+
+    def test_that_getting_the_right_effective_words(self):
+        words = ["Blue", "White", "Aqua", "Azure", "Beige", "Lavender"]
+        effective_words = get_effective_words(words, 1, 3)
+        self.assertEquals(["Blue", "White", "Aqua"], effective_words)
+
+    def test_that_most_frequency_returns_the_right_words(self):
+        words = ["Blue", "White", "Blue", "White", "Beige", "Lavender", "Blue"]
+        most_frequency_words = get_most_frequency_words(words, 2)
+        self.assertEquals(2, len(most_frequency_words))
+        self.assertEquals("Blue", most_frequency_words[0][0])
+        self.assertEquals(3, most_frequency_words[0][1])
+        self.assertEquals("White", most_frequency_words[1][0])
+        self.assertEquals(2, most_frequency_words[1][1])
+
